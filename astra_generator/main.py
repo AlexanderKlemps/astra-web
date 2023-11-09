53b44edc8821a1fb1744c3f3cb712345fa2fafad
Alexander Klemps <alexander.klemps@hotmail.com>
2023-11-09 16:01:22 +0100

    Added nginx container and compose files for remote deployments.

--- astra_generator/main.py
+++ astra_generator/main.py
@@ -1,11 +1,11 @@
 import os
-from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi import FastAPI, Depends, HTTPException, status, APIRouter
 from fastapi.security import OAuth2PasswordBearer
 from .generator.schemas import Input, Output
 from .generator.generator import write_input_file, process_generator_input, read_output_file
 
 
-API_KEYS = [os.environ["API_KEY"]]
+API_KEYS = [os.getenv("API_KEY")]
 
 # API key authorization taken from here 
 # https://testdriven.io/tips/6840e037-4b8f-4354-a9af-6863fb1c69eb/
@@ -20,8 +20,7 @@ def api_key_auth(api_key: str = Depends(oauth2_scheme)):
         )
 
 
-app = FastAPI()
-
+app = FastAPI(root_path=os.getenv("ROOT_PATH", "/"))
 
 @app.post("/generate")  # dependencies=[Depends(api_key_auth)])
 async def generate(generator_input: Input) -> Output:
@@ -30,4 +29,3 @@ async def generate(generator_input: Input) -> Output:
     process_output = read_output_file(generator_input)
 
     return process_output
-
